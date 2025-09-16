8292f4cc12534bae8847d42ec96bca7a1914e114
guyt6517b <maasben@usd437.net>
2025-09-16 15:47:16 +0000

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
 from app import app
 
-# This exposes the Flask app to Render's WSGI server
-application = app
-
-# Optional: if you want to run standalone:
 if __name__ == "__main__":
-    application.run()
+    app.run()
